f3170b856bc1589c87628b94f76cb021e8fa3aef
Hernani <hernani.luiz@gmail.com>
2020-04-21 21:17:44 -0300

    init

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import reduxThunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { BrowserRouter } from 'react-router-dom';
+import { reducers } from './store';
+import HomePage from './pages/HomePage';
+import './App.css';
+
+const { usersReducer, postsReducer } = reducers;
+
+const reducer = combineReducers({
+  usersReducer,
+  postsReducer,
+});
+const store = createStore(
+  reducer,
+  composeWithDevTools(applyMiddleware(reduxThunk)),
+);
+
+function App() {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <div
+          className="App"
+          style={{
+            padding: '1rem',
+            minWidth: '1px',
+          }}>
+          <HomePage />
+        </div>
+      </BrowserRouter>
+    </Provider>
+  );
+}
+
+export default App;
